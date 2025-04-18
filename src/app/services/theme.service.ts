2423d9efa84a202a78cef95bf153a6a5e195147c
AngeliniAxel <axel.angelini@gmail.com>
2025-04-18 19:06:32 +0200

    Add BehaviorSubject to manage theme state and enable reactive updates

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 type Theme = 'light' | 'dark';
 
@@ -6,13 +7,22 @@ type Theme = 'light' | 'dark';
   providedIn: 'root',
 })
 export class ThemeService {
-  currentTheme: Theme = 'light';
+  // A BehaviorSubject to hold the current theme ('light' or 'dark')
+  // It initializes with the value from localStorage or defaults to 'dark'
+  private themeSubject = new BehaviorSubject<Theme>(
+    (localStorage.getItem('theme') as Theme) || 'dark'
+  );
 
-  getTheme(): Theme {
-    return this.currentTheme;
+  // Exposes the BehaviorSubject so components can subscribe to theme changes
+  getTheme(): BehaviorSubject<Theme> {
+    return this.themeSubject;
   }
 
+  // Toggles the theme between 'light' and 'dark'
+  // Updates the BehaviorSubject and persists the new theme in localStorage
   toggleTheme(): void {
-    this.currentTheme === 'light' ? 'dark' : 'light';
+    const newTheme = this.themeSubject.value === 'light' ? 'dark' : 'light';
+    this.themeSubject.next(newTheme); // Notify subscribers of the new theme
+    localStorage.setItem('theme', newTheme);
   }
 }
