60bbdf7d693762ed39542496fb713c41a1d1dc09
AngeliniAxel <axel.angelini@gmail.com>
2025-04-18 19:06:55 +0200

    add suscription to theme changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ThemeService } from './services/theme.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,20 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'portfolio';
+  themeService = inject(ThemeService);
+  private themeSubscription!: Subscription; // Subscription to listen for theme changes
+
+  ngOnInit() {
+    // Subscribes to the theme changes from the ThemeService
+    this.themeSubscription = this.themeService.getTheme().subscribe((theme) => {
+      document.body.className = theme; // Updates the <body> class to reflect the current theme
+    });
+  }
+
+  ngOnDestroy() {
+    // Unsubscribes from the theme observable to prevent memory leaks
+    if (this.themeSubscription) {
+      this.themeSubscription.unsubscribe();
+    }
+  }
 }
