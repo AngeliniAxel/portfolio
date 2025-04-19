665cb3f9d9a04a229789f41a4de228cf201156f8
AngeliniAxel <axel.angelini@gmail.com>
2025-04-19 19:07:27 +0200

    handle language from toggle btns

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../services/theme.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'navbar',
@@ -9,7 +10,41 @@ import { ThemeService } from '../../services/theme.service';
 })
 export class NavbarComponent {
   themeService = inject(ThemeService);
-  onClick() {
-    this.themeService.toggleTheme();
+  currentTheme = '';
+  currentLanguage: string = 'en';
+
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit() {
+    // Initializes the current theme from the ThemeService
+    this.currentTheme = this.themeService.getTheme().value;
+
+    // Initializes the current language from localStorage or defaults to 'en'
+    this.currentLanguage = localStorage.getItem('lang') || 'en';
+
+    // Sets the initial language for ngx-translate
+    this.translate.use(this.currentLanguage);
+
+    // Updates the <html> element's lang attribute for accessibility
+    document.documentElement.lang = this.currentLanguage;
+  }
+
+  toggleTheme() {
+    // Toggle the theme using ThemeService and update local state
+    this.currentTheme = this.themeService.toggleTheme().value;
+  }
+
+  toggleLanguage() {
+    // Toggle between English and Spanish
+    this.currentLanguage = this.currentLanguage === 'en' ? 'es' : 'en';
+
+    // Update ngx-translate language
+    this.translate.use(this.currentLanguage);
+
+    // Persist selected language in localStorage
+    localStorage.setItem('lang', this.currentLanguage);
+
+    // Update the <html> tag's lang attribute accordingly
+    document.documentElement.lang = this.currentLanguage;
   }
 }
