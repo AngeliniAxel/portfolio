45f82958ff35a8867ebe897740009fb1401b92ac
AngeliniAxel <axel.angelini@gmail.com>
2025-04-25 18:35:37 +0200

    removed router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ThemeService } from './services/theme.service';
 import { Subscription } from 'rxjs';
