e5eff40c64c033f3898ffa2a92398763215261b7
AngeliniAxel <axel.angelini@gmail.com>
2025-04-27 23:47:17 +0200

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  emailServiceId: process.env['NG_APP_EMAIL_SERVICE_ID'] || '',
-  emailTemplateId: process.env['NG_APP_EMAIL_TEMPLATE_ID'] || '',
-  emailPublicKey: process.env['NG_APP_EMAIL_PUBLIC_KEY'] || '',
+  emailServiceId: (window as any)['NG_APP_EMAIL_SERVICE_ID'],
+  emailTemplateId: (window as any)['NG_APP_EMAIL_TEMPLATE_ID'],
+  emailPublicKey: (window as any)['NG_APP_EMAIL_PUBLIC_KEY'],
 };
