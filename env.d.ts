3c4245f642680747321d8646e8bec906092ac598
AngeliniAxel <axel.angelini@gmail.com>
2025-04-28 08:44:27 +0200

    update environment

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,11 @@
-declare const NG_APP_EMAIL_SERVICE_ID: string;
-declare const NG_APP_EMAIL_TEMPLATE_ID: string;
-declare const NG_APP_EMAIL_PUBLIC_KEY: string;
+/// <reference types="@ngx-env/core" />
+
+interface ImportMetaEnv {
+  readonly NG_APP_EMAIL_SERVICE_ID: string;
+  readonly NG_APP_EMAIL_TEMPLATE_ID: string;
+  readonly NG_APP_EMAIL_PUBLIC_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  emailServiceId: NG_APP_EMAIL_SERVICE_ID,
-  emailTemplateId: NG_APP_EMAIL_TEMPLATE_ID,
-  emailPublicKey: NG_APP_EMAIL_PUBLIC_KEY,
+  emailServiceId: import.meta.env.NG_APP_EMAIL_SERVICE_ID,
+  emailTemplateId: import.meta.env.NG_APP_EMAIL_TEMPLATE_ID,
+  emailPublicKey: import.meta.env.NG_APP_EMAIL_PUBLIC_KEY,
 };
