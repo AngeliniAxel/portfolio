141b7e63ef6c4f8c77cb9dcd79925d68633691fe
AngeliniAxel <axel.angelini@gmail.com>
2025-04-28 08:37:05 +0200

    add .env to ts.config

---
+++ env.d.ts
@@ -0,0 +1,3 @@
+declare const NG_APP_EMAIL_SERVICE_ID: string;
+declare const NG_APP_EMAIL_TEMPLATE_ID: string;
+declare const NG_APP_EMAIL_PUBLIC_KEY: string;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  emailServiceId: '${NG_APP_EMAIL_SERVICE_ID}',
-  emailTemplateId: '${NG_APP_EMAIL_TEMPLATE_ID}',
-  emailPublicKey: '${NG_APP_EMAIL_PUBLIC_KEY}',
+  emailServiceId: NG_APP_EMAIL_SERVICE_ID,
+  emailTemplateId: NG_APP_EMAIL_TEMPLATE_ID,
+  emailPublicKey: NG_APP_EMAIL_PUBLIC_KEY,
 };
