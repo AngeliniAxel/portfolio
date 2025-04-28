e417dbc74e99b3acefcc2e18d911307cbb8549de
AngeliniAxel <axel.angelini@gmail.com>
2025-04-28 10:41:51 +0200

    add environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  emailServiceId: 'NG_APP_EMAIL_SERVICE_ID',
+  emailTemplateId: 'NG_APP_EMAIL_TEMPLATE_ID',
+  emailPublicKey: 'NG_APP_EMAIL_PUBLIC_KEY',
+};
